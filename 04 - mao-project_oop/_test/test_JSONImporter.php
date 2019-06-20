539a72fe033ad22c490f0a207bf2859bffc4369f
sarassoroberto <sarassoroberto@gmail.com>
2019-06-20 10:49:10 +0200

    Test driven - scritto il primo test non da errori visibili rispetto a quelli preventivati

--- 04 - mao-project_oop/_test/test_JSONImporter.php
+++ 04 - mao-project_oop/_test/test_JSONImporter.php
@@ -1,3 +1,19 @@
 <?php
 require "../autoload.php";
+$files = [
+    "./json/file_vuoto.json",
+    "./json/file_manca_valore.json",
+    "./json/file_non_valido_immagine.json",
+    "./json/file_non_valido_virgola.json",
+    "./json/file_valido.json"
+];
 
+
+
+$ji = new JSONImporter();
+
+
+foreach ($files as $file) {
+
+    $ji->open($file);
+}
