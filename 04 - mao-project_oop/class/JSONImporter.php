5ea49bac58a8970acf48dcbe5d0cecda137aa531
sarassoroberto <sarassoroberto@gmail.com>
2019-06-20 22:20:55 +0200

    Inserita gestione degli errori per il formato del json JSONImporter  modifica del metoto JSON::open()

--- 04 - mao-project_oop/class/JSONImporter.php
+++ 04 - mao-project_oop/class/JSONImporter.php
@@ -1,4 +1,6 @@
 <?php
+
+
 class JSONImporter
 {
     private $dataset = '';
@@ -6,9 +8,18 @@ class JSONImporter
     {
         if (file_exists($source)) {
             $string = file_get_contents($source);
-            $this->dataset = json_decode($string);
         } else {
             throw new Exception('non riesco a trovare la risorsa $source');
+            throw new Exception("non riesco a trovare la risorsa $source");
+        }
+
+        /** Gestione degli errori di parsing del json 
+         *  @see https://laravel-news.com/php-7-3-json-error-handling
+         *  @see https://www.php.net/manual/en/function.json-last-error.php
+         */
+        $this->dataset = json_decode($string);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('errore nel formato del json');
         }
 
         return $this;
