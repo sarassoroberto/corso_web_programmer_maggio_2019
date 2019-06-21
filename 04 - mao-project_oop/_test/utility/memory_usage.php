376c9cd223f3364c3140c5570fcc01b5df3d1554
sarassoroberto <sarassoroberto@gmail.com>
2019-06-21 07:49:35 +0200

    Memory - usage - cambiata unita di misura

--- 04 - mao-project_oop/_test/utility/memory_usage.php
+++ 04 - mao-project_oop/_test/utility/memory_usage.php
@@ -2,6 +2,7 @@
 /**
  * funzione per ottenere l'occupazione in memoria di uno script
  * fonte: @see https://alexwebdevelop.com/monitor-script-memory-usage/
+ * @see https://stackoverflow.com/questions/5501427/php-filesize-mb-kb-conversion/5501447
  * @return void
  */
 function print_mem(): void
@@ -12,6 +13,8 @@ function print_mem(): void
     /* Peak memory usage */
     $mem_peak = memory_get_peak_usage();
 
-    echo 'The script is now using: [' . round($mem_usage / 1024) . 'KB] of memory.<br>';
-    echo 'Peak usage: [' . round($mem_peak / 1024) . 'KB] of memory.\n\n';
+    echo "\n - now: " . round($mem_usage / (1024)) . " KB\n";
+    echo " - peak: " . round($mem_peak / (1024)) . " KB\n\n";
 }
+/**
+ */
