f87c4e4e3e33a96cf5e6076cf590cdc40b27e98e
sarassoroberto <sarassoroberto@gmail.com>
2019-06-21 07:48:57 +0200

    ImageDownloader - Abbozzato controllo di validita dell'url

--- 04 - mao-project_oop/class/ImageDownloader.php
+++ 04 - mao-project_oop/class/ImageDownloader.php
@@ -5,11 +5,28 @@ class ImageDownloader
     private $source;
     public function open($source): void
     {
+        $this->source = $source;
         $this->imagedata = file_get_contents($source);
     }
 
     public function save($location): void
     {
         file_put_contents($location, $this->imagedata);
+    }
+
+    /**
+     * controlla la validità di un url
+     * @todo  refactoring, spostare in una classe di validazione 
+     * @see https://php.net/manual/en/reserved.variables.httpresponseheader.php
+     * @param string $url
+     * @return void
+     */
+    private function testUrl($url)
+    {
+        $header = get_headers($url);
+        if ($header[0] == 'HTTP/1.1 200 OK') {
+
+            print_r($header);
+        }
     }
 }
