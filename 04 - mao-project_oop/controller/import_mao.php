e89ef8e539eb82010a33195b9285f14fda346734
sarassoroberto <sarassoroberto@gmail.com>
2019-07-03 17:14:56 +0200

    Creazione Dell importer su DB e eliminazione vecchio importer incompleto

--- 04 - mao-project_oop/controller/import_mao.php
+++ 04 - mao-project_oop/controller/import_mao.php
@@ -1,12 +1,12 @@
 <?php
-include "autoload.php";
+include "../config.php";
+include "../autoload.php";
 
 $ji = new JSONImporter();
 
 $ji->open("https://www.fondazionetorinomusei.it/sites/default/files/allegati/COLLEZIONI_MAO.json");
 
 $dataset = $ji->getDataset();
-
 
 foreach ($dataset as $opera) {
       
