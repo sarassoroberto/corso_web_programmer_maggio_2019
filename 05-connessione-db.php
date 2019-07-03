d4e010e460c07824502570c6cecb9bd794cfeb8d
sarassoroberto <sarassoroberto@gmail.com>
2019-07-03 16:59:25 +0200

    Connessione DB

--- 05-connessione-db.php
+++ 05-connessione-db.php
@@ -6,24 +6,25 @@ try {
     $username = 'root';
     $password='';
     $unix_socket = '/var/mysql/mysql.sock';
+    $port=3306;
     // mysql:unix_socket=/tmp/mysql.sock;dbname=testdb
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
 
     // form 
-    $opera->Autore = ";DROP TABLE;";
-    // $sth = $pdo->prepare("select * from opera");
-    $sql = "INSERT INTO opera (Autore, Titolo, Datazione, Tecnica, Dimensioni, Immagine) 
-            VALUES (:Autore, :, 'XXX', 'mista', '10x2', 'gatto.jpg');";
+    // $opera->Autore = ";DROP TABLE;";
+    // // $sth = $pdo->prepare("select * from opera");
+    // $sql = "INSERT INTO opera (Autore, Titolo, Datazione, Tecnica, Dimensioni, Immagine) 
+    //         VALUES (:Autore, :, 'XXX', 'mista', '10x2', 'gatto.jpg');";
 
-    $pdo->prepare($sql);
-    // assegna i parametri
+    // $pdo->prepare($sql);
+    // // assegna i parametri
     
     // eseguire la query
-    $sth->execute();
-    $res = $sth->fetchAll(PDO::FETCH_CLASS,'Opera');
+    // $sth->execute();
+    // $res = $sth->fetchAll(PDO::FETCH_CLASS,'Opera');
 
-    print_r($res);
+    // print_r($res);
 }catch(PDOException $e)
 {
     echo $e->getMessage();
