196f61ef1d69afc4e976c235958f96153cab24e0
sarassoroberto <sarassoroberto@gmail.com>
2019-07-10 16:54:42 +0200

    inserita directory di upload file nella configurazione

--- 04 - mao-project_oop/config.php
+++ 04 - mao-project_oop/config.php
@@ -3,7 +3,10 @@
  define('DB_HOST','localhost');
  define('DB_NAME','museo');
  define('DB_USER','root');
- define('DB_PASSWORD','');
+//  define('DB_PASSWORD',''); delpho
+ define('DB_PASSWORD','fiacca'); //ufficio 
  define('DB_PORT',3306);
-
  
+ 
+ define('UPLOAD_DIR',__DIR__.'/img/originali/');
+
