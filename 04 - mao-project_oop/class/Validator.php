49ce509183a216a83fe9959b6823a0f280e79436
sarassoroberto <sarassoroberto@gmail.com>
2019-07-10 18:11:16 +0200

    Validazione nuova

--- 04 - mao-project_oop/class/Validator.php
+++ 04 - mao-project_oop/class/Validator.php
@@ -1,14 +1,49 @@
-<?php 
-class Validator 
-{
-    public static function base($data)
+<?php
+/**
+ * classe di validazione generale 
+ * Articoli interessanti
+ * @see https://www.wordfence.com/learn/how-to-write-secure-php-code/
+ * @see https://www.w3schools.com/php7/php7_form_validation.asp
+ */
+class Validator{
+    /**
+     * pulizia di base elimina gli spazi e converte i caratteri 
+     * speciali in entità html e gli slash.
+     *
+     * @param [type] $data
+     * @return void
+     */
+    public static function base($data):string
     {
-        return htmlspecialchars(trim($data));
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-
+    /**
+     * elimina i tag da una stringa
+     *
+     * @param string $data
+     * @return string
+     */
+    public static function striptag($data):string
+    {
+        $data = strip_tags($data);
+        return $data;
+    }
+    /**
+     * controlla se il valore di un for e vuoto
+     *
+     * @param string $data
+     * @return string
+     */
     public static function required($data):bool
     {
-        //false se  il cmapo non viene compilato
-        return self::base($data) != '';
+        $data = self::base($data);
+        var_dump(empty($data));
+        // empty restituisce true se la stringa e nulla 
+        // empty('') --> true, empty(' ') --> false,  empty('ciao') --> false
+        // quindi per avere l'effetto voluto devo negare con !
+        return !empty($data);
     }
 }
